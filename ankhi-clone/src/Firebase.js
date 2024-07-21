f53ff9fb866e85c55b749ffd464c976eae1cba1a
2Row <156104990+simonthurow@users.noreply.github.com>
2024-07-21 14:21:32 +0200

    Update Firebase.js

--- ankhi-clone/src/Firebase.js
+++ ankhi-clone/src/Firebase.js
@@ -4,13 +4,13 @@ import{
 }from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjTgWiEHO08E3lmii12x5SCSTv7_qxmXU",
-  authDomain: "ankhi-clone-3c01d.firebaseapp.com",
-  projectId: "ankhi-clone-3c01d",
-  storageBucket: "ankhi-clone-3c01d.appspot.com",
-  messagingSenderId: "200522436464",
-  appId: "1:200522436464:web:6ab4783d3b32774c4f89b5",
-  measurementId: "G-L869D2CK79"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 
